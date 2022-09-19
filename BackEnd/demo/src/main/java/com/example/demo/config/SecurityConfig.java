0bbe863570a7c54cfad97440bc7573cdc9d9786d
itmdeveloper <choidasol95@gmail.com>
2022-09-19 23:51:11 +0900

    feat: JWT 토큰 발급, 테스트 용으로 완성됨.

--- BackEnd/demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ BackEnd/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,15 +2,24 @@ package com.example.demo.config;
 
 
 import com.example.demo.entity.role.Role;
+import com.example.demo.handler.OAuth2SuccessHandler;
 import com.example.demo.service.CustomOAuth2UserService;
+import com.example.demo.service.TokenService;
+import com.example.demo.util.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -19,8 +28,13 @@ import org.springframework.security.web.DefaultSecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2SuccessHandler successHandler;
+    private final TokenService tokenService;
 
-
+    @Bean
+    public BCryptPasswordEncoder encodePWD() {
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     public WebSecurityCustomizer configure(){
         return (web) -> web.ignoring().antMatchers(
@@ -32,13 +46,15 @@ public class SecurityConfig {
 
     @Bean
     public DefaultSecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-       return http.csrf().disable().headers()
+       return http
+               .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+               .and()
+               .csrf().disable().headers()
                .and()
                // authorizeRequests() : URL 별로 권한 관리를 설정하는 옵션
                // antMatchers('/url', ..) : 권한 관리 지정 옵션, URL과 HTTP 메소드별 관리, 해당 메소드 뒤의 체인은 열람 권한 설정
                .authorizeRequests().antMatchers("/", "/css/**","/images/**","/js/**")
                .permitAll()
-
                 // '/api/아래 요청이 오면 인증이 필요하게 됨
                 .antMatchers("/api/**").hasRole(Role.USER.name())
                 .and()
@@ -52,8 +68,11 @@ public class SecurityConfig {
                //로그아웃 성공 시 / 주소로 이동
                .logoutSuccessUrl("/")
                .and()
+               .addFilterBefore(new JwtAuthFilter(tokenService),
+                       OAuth2LoginAuthenticationFilter.class)
                //oauth 로그인 기능에 대한 설정의 진입점
                .oauth2Login()
+               .successHandler(successHandler)
                //oauth 로그인 성공 이후 사용자의 정보를 가져올 때 설저 담당
                .userInfoEndpoint()
                //소셜 로그인 성공 시 후속 조치
--- BackEnd/demo/src/main/java/com/example/demo/dto/UserDTO.java
+++ BackEnd/demo/src/main/java/com/example/demo/dto/UserDTO.java
@@ -1,5 +1,20 @@
 package com.example.demo.dto;
 
-public class UserDTO {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@Getter
+public class UserDTO {
+    private String email;
+    private String name;
+    private String picture;
+
+    @Builder
+    public UserDTO(String email, String name, String picture) {
+        this.email = email;
+        this.name = name;
+        this.picture = picture;
+    }
 }
