0462f2888e2a7eaea23b93d16cada6fe8d638685
MIRAE-JUNGAH <jungahzzzang@gmail.com>
2022-11-09 10:55:48 +0900

    fix:pull 오류 해결

--- FrontEnd/src/navigations/Index.js
+++ FrontEnd/src/navigations/Index.js
@@ -1,11 +1,16 @@
 import React, {useContext} from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AuthStack from './AuthStack';
+<<<<<<< Updated upstream
+=======
+import MainStack from './MainStack';
+import * as Font from 'expo-font';
+>>>>>>> Stashed changes
 
 const Navigation = () => {
     return(
         <NavigationContainer>
-           <AuthStack />
+           <MainStack />
         </NavigationContainer>
     );
 };
